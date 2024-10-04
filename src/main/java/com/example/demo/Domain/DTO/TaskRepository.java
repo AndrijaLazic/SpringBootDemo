63c97b1c10196dead017bf9e051df08a94bf86d3
AndrijaLazic <lazic.andrija223@gmail.com>
2024-10-04 19:36:32 +0200

    Change from stored procedures to queries

--- src/main/java/com/example/demo/Domain/DTO/TaskRepository.java
+++ src/main/java/com/example/demo/Domain/DTO/TaskRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.Domain.DTO;
 
 import com.example.demo.Domain.DatabaseEntity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
@@ -9,13 +10,20 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    @Procedure("sp_get_tasks_between_dates")
+    @Query("SELECT t FROM Task t " +
+           "JOIN Activity a ON t.id = a.task.id " +
+           "WHERE t.frequency = :frequency AND t.section = :section AND (a.date BETWEEN :dateStart AND :dateEnd)")
     List<Task> getDoneTasksBetweenDates(@Param("dateStart") LocalDate dateStart,
-                                        @Param("dateEnd") LocalDate dateEnd,
-                                        @Param("frequency") String frequency,
-                                        @Param("section") String section);
+                                    @Param("dateEnd") LocalDate dateEnd,
+                                    @Param("frequency") String frequency,
+                                    @Param("section") String section);
 
-    @Procedure("sp_get_todo_tasks_between_dates")
+    @Query("SELECT t1 FROM Task t1 " +
+            "WHERE t1.id NOT IN (" +
+                "SELECT t.id FROM Task t " +
+                "JOIN Activity a ON t.id = a.task.id " +
+                "WHERE a.date BETWEEN :dateStart AND :dateEnd" +
+            ") AND t1.frequency = :frequency AND t1.section = :section")
     List<Task> getTODOTasksBetweenDates(@Param("dateStart") LocalDate dateStart,
                                         @Param("dateEnd") LocalDate dateEnd,
                                         @Param("frequency") String frequency,
